6d189224e896594f2b38ddc792dd138ad7c6cf09
Gabigabe <mhammed.gabigabe@gmail.com>
2026-01-28 10:12:33 +0100

    modification sur Profil

--- app/Models/Profil.php
+++ app/Models/Profil.php
@@ -6,6 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Profil extends Model
 {
+   
+    // protected $fillable = [
+    //     'user_id',
+    //     'photo',
+    // ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
     public function recettes()
     {
         return $this->hasMany(Recette::class);
